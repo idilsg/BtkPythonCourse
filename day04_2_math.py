f287814eb367f3617b6734a699ce841fa7266b92
İdil Gürsoy <64634611+idilsg@users.noreply.github.com>
2025-01-30 12:37:42 +0300

    math 1

---
+++ day04_2_math.py
@@ -0,0 +1,27 @@
+import math
+# import math as m
+from math import pi as pi_sayisi, sqrt as karekok
+
+a = math.factorial(5)
+print(a)  # 120
+
+b = math.sin(math.radians(90))
+print(b)  # 1
+
+c = math.pi
+print(c)  # 3.141592653589793
+
+d = math.pow(2, 5)
+print(d)  # 32.0
+
+e = math.sqrt(16)
+print(e)  # 4.0
+
+x = pi_sayisi
+y = karekok(16)
+print(round(x, 4), y)  # 3.1416 4.0
+
+
+
+
+
