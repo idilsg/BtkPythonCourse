315c228a9acde3a2a86517dcef58fd235da536fc
İdil Gürsoy <64634611+idilsg@users.noreply.github.com>
2025-01-30 15:10:30 +0300

    random sayı

--- day04_2_math.py
+++ day04_2_math.py
@@ -21,7 +21,76 @@ x = pi_sayisi
 y = karekok(16)
 print(round(x, 4), y)  # 3.1416 4.0
 
+print("\n")
 
+import random
 
+print(random.randint(1, 100))  # 1-100 arası rastgele sayı (1 ve 100 dahil)
+print(random.random())  # örn: 0.7885570290400251
+print(random.randrange(0, 100, 5))  # 0-100 arası 5 aralıklı rastgele sayı (10 dahil değil)
 
+print("\n")
 
+# sayı tahmin etme oyunu
+print("Sayı Tahmin Etme Oyunu")
+baslangic = 0
+bitis = 100
+tutulan_sayi = random.randint(baslangic, bitis)
+tahmin_sayisi = 5
+taban = baslangic
+tavan = bitis
+
+while tahmin_sayisi > 0:
+    istek = f"{taban} - {tavan} aralığında bir sayı giriniz: "
+    tahmin = int(input(istek))
+    if tahmin == tutulan_sayi:
+        print(f"Tebrikler. Tutulan sayı: {tutulan_sayi}")
+        break
+    else:
+        tahmin_sayisi -= 1
+        if tahmin_sayisi == 0:
+            print(f"Tahmin hakkınız doldu. Tutulan sayı: {tutulan_sayi}")
+            break
+        elif tahmin > tutulan_sayi:
+            print("Daha küçük bir sayı giriniz.")
+            tavan = tahmin
+        elif tahmin < tutulan_sayi:
+            print("Daha büyük bir sayı giriniz.")
+            taban = tahmin
+        print(f"Kalan tahmin hakkınız: {tahmin_sayisi}")
+
+print("\n")
+
+# sayı tahmin ettirme oyunu
+print("Sayı Tahmin Ettirme Oyunu")
+
+baslangic = 0
+bitis = 100
+tutulan_sayi = 0
+check = False
+
+# sayı tutma bloğu
+while not check:
+    tutulan_sayi = int(input(f"{baslangic}-{bitis} arasında bir sayı tutunuz: "))
+    if tutulan_sayi > bitis or tutulan_sayi < baslangic:
+        check = False
+        print("Belirtilen aralıkta bir sayı girmediniz.")
+    else:
+        check = True
+
+print("---")
+correct = False
+taban = baslangic
+tavan = bitis
+while not correct:
+    tahmin = random.randint(taban, tavan)
+    print(f"Tahmin edilen sayı: {tahmin}")
+    if tahmin == tutulan_sayi:
+        correct = True
+        print(f"Buldunuz. Tutulan sayı: {tutulan_sayi}")
+    elif tahmin > tutulan_sayi:
+        print("Daha küçük bir sayı giriniz.")
+        tavan = tahmin
+    elif tahmin < tutulan_sayi:
+        print("Daha büyük bir sayı giriniz.")
+        taban = tahmin
