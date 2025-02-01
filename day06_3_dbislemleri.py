a2598fa63087492057986afa58c989227460e23b
İdil Gürsoy <64634611+idilsg@users.noreply.github.com>
2025-02-01 15:38:22 +0300

    db update func

--- day06_3_dbislemleri.py
+++ day06_3_dbislemleri.py
@@ -13,7 +13,7 @@ def birim_listele():
 
 
 def birim_ekle():
-    birimAdi = input("Birim adı giriniz: ")
+    birimAdi = input("\nBirim adı giriniz: ")
     yeni_birim = Birim(birimAdi=birimAdi)
     session.add(yeni_birim)
     session.commit()
@@ -21,19 +21,31 @@ def birim_ekle():
 
 def birim_sil():
     birim_listele()
-    birimId = int(input("Silinecek birim id giriniz: "))
+    birimId = int(input("\nSilinecek birim id giriniz: "))
     birim = session.query(Birim).filter(Birim.birimId == birimId).delete()
     session.commit()
     return birim_listele()
 
 
+def birim_guncelle():
+    birim_listele()
+    birimId = int(input("\nGüncellemek istediğiniz birimin id'sini giriniz: "))
+    birim = session.query(Birim).filter(Birim.birimId == birimId).first()
+    yeni_birim_adi = input("\nYeni birim adını giriniz: ")
+    birim.birimAdi = yeni_birim_adi
+    session.commit()
+    return birim_listele()
+
+
 while True:
-    cevap = input("e, l, s, x ")
+    cevap = input("\ne, l, s, g, x ")
     if cevap == "e":
         birim_ekle()
     elif cevap == "l":
         birim_listele()
     elif cevap == "s":
         birim_sil()
+    elif cevap == "g":
+        birim_guncelle()
     elif cevap == "x":
         exit()
